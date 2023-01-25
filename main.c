ef4e4845b061678e00104316fa2721ecafd2d333
Alexandre Couture <acouture@c2r4p8.42quebec.com>
2023-01-25 16:32:30 -0500

    Update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: acouture <acouture@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 13:53:42 by acouture          #+#    #+#             */
-/*   Updated: 2023/01/25 14:21:13 by acouture         ###   ########.fr       */
+/*   Updated: 2023/01/25 16:30:22 by acouture         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int main ()
     int fd;
 
     fd = open("test.txt", O_RDONLY);
-    for (size_t i = 0; i < 3; i++)
+    for (size_t i = 0; i < 1; i++)
     {
         printf("%s", get_next_line(fd));
     }
